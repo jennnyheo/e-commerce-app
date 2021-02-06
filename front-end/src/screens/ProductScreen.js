5880dd83a0283aef25af15ba29a9c5a6f0d2ab99
jennnyheo <heejuheo13@gmail.com>
2021-02-07 01:10:26 +0900

    add redux to home screen

--- front-end/src/screens/ProductScreen.js
+++ front-end/src/screens/ProductScreen.js
@@ -50,7 +50,7 @@ export default function ProductScreen(props) {
                                     {product.countInStock > 0 ? (
                                         <span className="success">In stock</span>
                                     ) : (
-                                        <span className="error">Unavilable</span>
+                                        <span className="danger">Unavilable</span>
                                     )}
 
                                 </div>
