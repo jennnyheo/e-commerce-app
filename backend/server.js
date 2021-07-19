7be9d339584a851fcacbd9925fb158d4975b1793
jennnyheo <heejuheo13@gmail.com>
2021-07-20 00:37:39 +0900

    products moved to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
-import data from "./data.js";
+import productRouter from "./router/productRouter.js";
 import userRouter from "./router/userRouter.js";
 
 const app = express();
@@ -10,26 +10,14 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/amazona", {
   useCreateIndex: true,
 });
 
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
 app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
-
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
